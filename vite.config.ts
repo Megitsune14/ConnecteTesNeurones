68298b4136ee5828e94bc55ddc19ee493d11c607
Megitsune <megitsune14.pro@gmail.com>
2026-06-04 17:28:23 +0200

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+    allowedHosts: ['connectetesneurones.onrender.com'],
+  },
 })
